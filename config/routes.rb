89cfa3aef45ce7c70fd07e355197ef2ff5ec8118
Alan Vincent <alan.d.vincent@gmail.com>
2017-06-10 14:27:12 -0600

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,12 @@
 Rails.application.routes.draw do
-  get 'portfolios/index'
 
-  get 'portfolios/show'
-
-  get 'portfolios/exit'
-
-  get 'artists/index'
-
-  get 'artists/show'
-
-  get 'artists/exit'
-
-  get 'home/index'
 
   devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :artists
+  resources :artists do
   resources :portfolios
+  end
 
 root 'home#index'
 end
