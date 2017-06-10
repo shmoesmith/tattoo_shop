c5aec589974836b28efa5b45804a15271e132488
Catherine Callow-Heusser <ccallowheusser@gmail.com>
2017-06-10 11:06:55 -0600

    added resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :artists
+  resources :portfolios
+
 root 'home#index'
 end
