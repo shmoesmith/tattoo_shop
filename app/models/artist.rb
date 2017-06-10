49c630d06e4ffc145c9df181539c9f5a1e9e7f40
Catherine Callow-Heusser <ccallowheusser@gmail.com>
2017-06-10 11:14:58 -0600

    models

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,7 @@
 class Artist < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :shop
+
+  has_many :portfolio, dependent: :destroy
+
 end
--- app/models/porfolio.rb
+++ app/models/porfolio.rb
@@ -1,2 +1,6 @@
 class Porfolio < ApplicationRecord
+  validates_presence_of :style
+
+  belongs_to :doctor
+
 end
